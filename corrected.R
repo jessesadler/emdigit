0f6de36aa6d81a8a7f40de826b4e9a5cd60ee99a
Jesse Sadler <jrsadler@icloud.com>
2021-06-21 10:53:00 -0400

    Add corrected data

---
+++ corrected.R
@@ -0,0 +1,14 @@
+## Corrected data ##
+
+library(tidyverse)
+
+# Full data ---------------------------------------------------------------
+
+fdc <- read_csv("data/full_data_corrected.csv")
+fd <- read_csv("data/full-data.csv")
+
+
+# Route headings ----------------------------------------------------------
+
+rhc <- read_csv("data/route_headings_corrections.csv")
+rh <- read_csv("data/route-heading.csv")
