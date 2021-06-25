f7c509fda9386e415fda1670105f7c9d2849e1f8
Jesse Sadler <jrsadler@icloud.com>
2021-06-25 12:54:20 -0400

    Bounding boxes from polygon data
    * Shows min/max y value of tags
    * Shows zones that go across column divider
    * Plot of polygon facet wrap

--- tag-zones.R
+++ tag-zones.R
@@ -20,6 +20,8 @@ zones_plot <- function(tbl, scan) {
     ggplot2::theme_void()
 }
 
+# ggsave("plots/codogno-page70-zones.pdf")
+
 # sf polygons -------------------------------------------------------------
 
 # tbl: elements tbl from full-data-tbl
@@ -38,4 +40,49 @@ sf_polygons <- function(tbl) {
 }
 
 tbl_sf <- sf_polygons(tbl_elements)
+
+# Plot of all polygons
+ggplot(tbl_sf) +
+  geom_sf(aes(color = type), alpha = 0.5) +
+  facet_wrap(vars(type)) +
+  labs(title = "All tag zones by type")
+
+# ggsave("plots/tag-zones-facet.pdf")
+
+# Bounding boxes ----------------------------------------------------------
+
+# Bounding box for each polygon
+bboxes <- tbl_sf %>%
+  st_geometry() %>%
+  map(st_bbox) %>%
+  transpose() %>%
+  as_tibble() %>%
+  unnest(everything()) %>%
+  bind_cols(select(tbl_elements, id, img, type, data))
+
+# Width of bboxes to show ones that go into both columns
+# Problems with those over 1100
+bboxes %>%
+  filter(type != "header" & type != "catch-word") %>%
+  mutate(width = xmax - xmin) %>%
+  arrange(desc(width))
+
+# Boxes that go across the middle of the columns
+bboxes %>%
+  filter(type != "header",
+         xmin < 600 & xmax > 1400)
+
+# Division between header data and other data
+
+# Header and page number minimum y
+bboxes %>%
+  filter(type %in% c("header", "page-number")) %>%
+  group_by(type) %>%
+  slice_min(ymin, n = 3)
+
+# Max y of data
+bboxes %>%
+  filter(type %in% c("locations", "distances", "route-heading")) %>%
+  group_by(type) %>%
+  slice_max(ymax, n = 3)
 
