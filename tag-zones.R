4cda9ec851f5161e402121f1af438208ab21fd65
Jesse Sadler <jrsadler@icloud.com>
2021-06-25 12:51:29 -0400

    sf polygons function
    * Use this function in zones plot
    * Plot to show what it looks like

--- tag-zones.R
+++ tag-zones.R
@@ -10,6 +10,22 @@ zones_plot <- function(tbl, scan) {
 
   tbl %>%
     dplyr::filter(img == scan) %>%
+    sf_polygons() %>%
+    # Plot
+    ggplot2::ggplot() +
+    ggplot2::geom_sf(aes(color = type, fill = type), alpha = 0.4) +
+    ggrepel::geom_text_repel(aes(label = id, geometry = geometry),
+                    stat = "sf_coordinates") +
+    ggplot2::labs(title = paste0("Page ", scan)) +
+    ggplot2::theme_void()
+}
+
+# sf polygons -------------------------------------------------------------
+
+# tbl: elements tbl from full-data-tbl
+
+sf_polygons <- function(tbl) {
+  tbl %>%
     # Separate pts data
     tidyr::separate_rows(pts, sep = " ") %>%
     tidyr::separate(pts, c("x", "y"), sep = ",", convert = TRUE) %>%
@@ -18,14 +34,8 @@ zones_plot <- function(tbl, scan) {
     sf::st_as_sf(coords = c("x", "y")) %>%
     dplyr::group_by(id, type) %>%
     dplyr::summarise(.groups = "drop") %>%
-    sf::st_convex_hull() %>%
-    # Plot
-    ggplot2::ggplot() +
-    ggplot2::geom_sf(aes(color = type, fill = type), alpha = 0.4) +
-    ggrepel::geom_text_repel(aes(label = id, geometry = geometry),
-                    stat = "sf_coordinates") +
-    ggplot2::theme_void()
-
+    sf::st_convex_hull()
 }
 
+tbl_sf <- sf_polygons(tbl_elements)
 
